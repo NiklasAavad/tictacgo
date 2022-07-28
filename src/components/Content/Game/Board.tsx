8b28568c93c065ca56e784be6d81cdb7f2e26786
Niklas <niklasaavad@gmail.com>
2022-07-29 00:02:16 +0200

    Xs and Os can be added to squares. Overlapping borders are fixed

--- src/components/Content/Game/Board.tsx
+++ src/components/Content/Game/Board.tsx
@@ -1,10 +1,85 @@
+import { useState } from "react"
 import "./Board.css"
-import { Row } from "./Row"
+import { Position } from "./Position"
+import { Border, Square, SquareCharacter } from "./Square"
+
+// const fakeX = [0, 2, 4]
+// const fakeO = [1, 6, 8]
 
 export const Board: React.FC = () => {
+    const [x, setX] = useState<Position[]>([]);
+    const [o, setO] = useState<Position[]>([]);
+    const [playerInTurn, setPlayerInTurn] = useState<SquareCharacter>(SquareCharacter.X);
+
+    const chooseSquare = (position: Position) => {
+        if (playerInTurn === SquareCharacter.X) {
+            const newX = [...x, position];
+            setX(newX);
+            setPlayerInTurn(SquareCharacter.O);
+        } else {
+            const newO = [...o, position];
+            setO(newO);
+            setPlayerInTurn(SquareCharacter.X);
+        }
+    }
+    
+    // TODO overvej om det her kan trækkes ud i en en service eller andet. Her skal createSquare dog bruge x og o, hvilket bliver til mange argumenter.
+    const createSquare = (position: Position, border: Border = {}) => {
+        const isX = x.includes(position);
+        const isO = o.includes(position);
+
+        return <Square isX={isX} isO={isO} border={border} position={position} chooseSquare={chooseSquare}/>
+    }
+
+    const createTopSquares = () => {
+        const topLeftBorders = {top: true, left: true};
+        const topCenterBorders = {top: true};
+        const topRightBorders = {top: true, right: true};
+
+        const topLeftSquare = createSquare(Position.TOP_LEFT, topLeftBorders);
+        const topCenterSquare = createSquare(Position.TOP_CENTER, topCenterBorders)
+        const topRightSquare = createSquare(Position.TOP_RIGHT, topRightBorders);
+
+        return [topLeftSquare, topCenterSquare, topRightSquare];
+    }
+    
+    const createCenterSquares = () => {
+        const centerLeftBorders = {left: true};
+        const centerBorders = {};
+        const centerRightBorders = {right: true};
+
+        const centerLeftSquare = createSquare(Position.CENTER_LEFT, centerLeftBorders);
+        const centerSquare = createSquare(Position.CENTER, centerBorders)
+        const centerRightSquare = createSquare(Position.CENTER_RIGHT, centerRightBorders);
+
+        return [centerLeftSquare, centerSquare, centerRightSquare]
+    }
+
+    const createBottomSquares = () => {
+        const bottomLeftBorders = {bottom: true, left: true};
+        const bottomCenterBorders = {bottom: true};
+        const bottomRightBorders = {bottom: true, right: true};
+
+        const bottomLeftSquare = createSquare(Position.BOTTOM_LEFT, bottomLeftBorders);
+        const bottomCenterSquare = createSquare(Position.BOTTOM_CENTER, bottomCenterBorders);
+        const bottomRightSquare = createSquare(Position.BOTTOM_RIGHT, bottomRightBorders);
+
+        return [bottomLeftSquare, bottomCenterSquare, bottomRightSquare];
+    }
+
+    const topSquares = createTopSquares();
+    const centerSquares = createCenterSquares();
+    const bottomSquares = createBottomSquares();
+
     return <div className="board">
-        <Row/>
-        <Row/>
-        <Row/>
+        <div className="row">
+            {topSquares}
+        </div>
+        <div className="row">
+            {centerSquares}
+        </div>
+        <div className="row">
+            {bottomSquares}
+        </div>
     </div>
 }
--- src/components/Content/Game/Square.tsx
+++ src/components/Content/Game/Square.tsx
@@ -1,5 +1,66 @@
+import { Position } from "./Position"
 import "./Square.css"
 
-export const Square: React.FC = () => {
-    return <div className="square"></div>
+export type Border = {
+    top?: boolean,
+    bottom?: boolean,
+    left?: boolean,
+    right?: boolean
+}
+
+type SquareProps = {
+    isX?: boolean,
+    isO?: boolean,
+    border: Border,
+    position: Position,
+    chooseSquare: (position: Position) => void
+}
+
+export enum SquareCharacter {
+    X = "X",
+    O = "O",
+    EMPTY = ""
+}
+
+enum BorderClass {
+    TOP = "top-border",
+    BOTTOM = "bottom-border",
+    LEFT = "left-border",
+    RIGHT = "right-border",
+    SEPERATOR = " "
+}
+
+// TODO overvej om der skal lægge noget state ifht om den her square er x eller o i dette komponent i stedet for i Board.
+// Board er ved at blive til et god-component
+export const Square: React.FC<SquareProps> = (props) => {
+    const getCharacter = () => {
+        if (props.isX) return SquareCharacter.X;
+        if (props.isO) return SquareCharacter.O;
+        return SquareCharacter.EMPTY;
+    }
+
+    const getBorderClasses = () => {
+        const border = props.border;
+        const borderClasses: BorderClass[] = []
+        
+        if (border.top) {
+            borderClasses.push(BorderClass.TOP);
+        }
+        if (border.bottom) {
+            borderClasses.push(BorderClass.BOTTOM);
+        }
+        if (border.left) {
+            borderClasses.push(BorderClass.LEFT);
+        }
+        if (border.right) {
+            borderClasses.push(BorderClass.RIGHT);
+        }
+
+        return borderClasses.join(BorderClass.SEPERATOR);
+    }
+
+    const character = getCharacter();
+    const borderClasses = getBorderClasses();
+
+    return <div onClick={() => props.chooseSquare(props.position)} className={"square " + borderClasses}>{character}</div>
 }

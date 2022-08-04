8e2617ecaac2e2c0587a692c09d2f282475f8232
Niklas <niklasaavad@gmail.com>
2022-08-05 01:08:58 +0200

    Simple websocket configurated

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// No checking for now, just allow any connection
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Print out the read message (for clarity)
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Simple Server")
+	})
+
+	http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
 	fmt.Println("Go App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }

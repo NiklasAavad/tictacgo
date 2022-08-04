7b43a35463f651dc1e5c44665a28abf2b7e44a01
Niklas <niklasaavad@gmail.com>
2022-08-04 23:08:10 +0200

    Semi intelligent bot added

--- frontend/src/service/OfflineSingleplayerGameService.ts
+++ frontend/src/service/OfflineSingleplayerGameService.ts
@@ -1,27 +1,60 @@
-import { getRandomItem } from "../utility/GameServiceUtility";
-import { Position, positions } from "../utility/Position";
+import { SquareCharacter } from "../components/Content/Game/Square/Square";
+import { getRandomItem, hasPlayerWon } from "../utility/GameServiceUtility";
+import { allPositions, Position } from "../utility/Position";
 import { GameContextMutator, GameService, Result } from "./GameService";
 import OfflineMultiplayerGameService from "./OfflineMultiplayerGameService";
 
 const OfflineSingleplayerGameService: GameService = (gameContextMutator: GameContextMutator) => {
+    let x: Position[] = [];
+    let o: Position[] = [];
+    let freePositions = allPositions;
     const decoratee = OfflineMultiplayerGameService(gameContextMutator);
+    let isBotChoosing = false;
+
+    const addSquareToBoard = (playerInTurn: SquareCharacter, position: Position) => {
+        if (playerInTurn === SquareCharacter.X) {
+            x.push(position);
+        } else {
+            o.push(position);
+        }
+        freePositions = freePositions.filter(freePosition => freePosition != position);
+    }
+
+    const choosePositionIfGameWouldEnd = (playerPositions: Position[]): Position | undefined => {
+        for (const freePosition of freePositions) {
+            const positionsIncludingFreePosition = [...playerPositions, freePosition];
+            if (hasPlayerWon(positionsIncludingFreePosition)) {
+                return freePosition;
+            }
+        }
+        return undefined;
+    }
 
     const choosePositionByBot = () => {
-        let position = undefined;
-        do {
-            position = getRandomItem(positions);
-        } while (position === undefined || !isChoiceValid(position));
+        const positionIfBotWouldWin = choosePositionIfGameWouldEnd(o);
+        if (positionIfBotWouldWin) {
+            return positionIfBotWouldWin;
+        }
 
-        return position;
+        const positionIfPlayerWouldWin = choosePositionIfGameWouldEnd(x);
+        if (positionIfPlayerWouldWin) {
+            return positionIfPlayerWouldWin;
+        }
+
+        return getRandomItem(freePositions);
     }
 
     const chooseSquareByBot = () => {
         const positionChosenByBot = choosePositionByBot();
         decoratee.chooseSquare(positionChosenByBot);
-        decoratee.changePlayerInTurn();
+        addSquareToBoard(SquareCharacter.O, positionChosenByBot);
+        changePlayerInTurn();
     }
 
     const startGame = (): void => {
+        freePositions = allPositions;
+        x = [];
+        o = [];
         decoratee.startGame();
     };
 
@@ -34,17 +67,26 @@ const OfflineSingleplayerGameService: GameService = (gameContextMutator: GameCon
     };
 
     const isChoiceValid = (position: Position): boolean => {
+        if (isBotChoosing) {
+            return false;
+        }
+
         return decoratee.isChoiceValid(position);
     };
 
     const chooseSquare = (position: Position): void => {
         decoratee.chooseSquare(position);
+        addSquareToBoard(SquareCharacter.X, position);
 
         if (isGameOver()) {
             return;
         }
 
-        setTimeout(() => chooseSquareByBot(), 500);
+        isBotChoosing = true;
+        setTimeout(() => {
+            chooseSquareByBot()
+            isBotChoosing = false;
+        }, 500);
     };
 
     const changePlayerInTurn = (): void => {
--- frontend/src/utility/Position.ts
+++ frontend/src/utility/Position.ts
@@ -1,10 +1,10 @@
 export enum Position {
-    TOP_LEFT, TOP_CENTER, TOP_RIGHT,
+    TOP_LEFT = 1, TOP_CENTER, TOP_RIGHT,
     CENTER_LEFT, CENTER, CENTER_RIGHT,
     BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT
 }
 
-export const positions = [
+export const allPositions = [
     Position.TOP_LEFT, Position.TOP_CENTER, Position.TOP_RIGHT,
     Position.CENTER_LEFT, Position.CENTER, Position.CENTER_RIGHT,
     Position.BOTTOM_LEFT, Position.BOTTOM_CENTER, Position.BOTTOM_RIGHT

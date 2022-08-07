58afe9638abcce7a901c5326814af780ad02d97f
Niklas <niklasaavad@gmail.com>
2022-08-07 17:20:55 +0200

    Created starting point for pool

---
+++ backend/pkg/websocket/pool.go
@@ -0,0 +1,66 @@
+package websocket
+
+import "fmt"
+
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool // forstår ikke helt typen her
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+func registerClient(p *Pool, c *Client) {
+	p.Clients[c] = true
+	fmt.Println("Size of Connection Pool: ", len(p.Clients))
+	msg := Message{Type: 1, Body: "New User Joined..."}
+	for client := range p.Clients {
+		fmt.Println(client)
+		client.Conn.WriteJSON(msg)
+	}
+}
+
+func unregisterClient(p *Pool, c *Client) {
+	delete(p.Clients, c)
+	fmt.Println("Size of Connection pool: ", len(p.Clients))
+	msg := Message{Type: 1, Body: "User Disconnected..."}
+	for client := range p.Clients {
+		client.Conn.WriteJSON(msg)
+	}
+}
+
+func broadcastMessage(p *Pool, msg Message) error {
+	fmt.Println("Sending message to all clients in Pool")
+	for client := range p.Clients {
+		if err := client.Conn.WriteJSON(msg); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			registerClient(pool, client)
+			break
+		case client := <-pool.Unregister:
+			unregisterClient(pool, client)
+			break
+		case message := <-pool.Broadcast:
+			if err := broadcastMessage(pool, message); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}

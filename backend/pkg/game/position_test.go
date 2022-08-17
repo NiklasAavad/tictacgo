2b9c9d7db94b5da3543d8757c85f3e99ee9b591a
Niklas <niklasaavad@gmail.com>
2022-08-18 00:28:17 +0200

    Testing unmarshal cases for Position

--- backend/pkg/game/position_test.go
+++ backend/pkg/game/position_test.go
@@ -1,6 +1,8 @@
 package game
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPositionShouldBeParsedForZero(t *testing.T) {
 	p, err := ParsePosition(0)
@@ -31,5 +33,38 @@ func TestShouldFailIfInputLowerThanZero(t *testing.T) {
 func TestShouldFailIfInputHeigherThanEight(t *testing.T) {
 	if _, err := ParsePosition(9); err == nil {
 		t.Errorf("Expected error with input position %d", 9)
+	}
+}
+
+func TestUnmarshalSucces(t *testing.T) {
+	var p Position
+	input := []byte{0x30}
+
+	if err := p.UnmarshalJSON(input); err != nil {
+		t.Fatal(err)
+	}
+
+	if p != TOP_LEFT {
+		t.Errorf("expected position to be TOP_LEFT, got %v", p)
+	}
+}
+
+func TestUnmarshalFailure(t *testing.T) {
+	var p Position
+	input := []byte{0x90}
+
+	err := p.UnmarshalJSON(input)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestUnmarshalFailureParsing(t *testing.T) {
+	var p Position
+	input := []byte{0x39}
+
+	err := p.UnmarshalJSON(input)
+	if err == nil {
+		t.Errorf("expected error, got nil")
 	}
 }

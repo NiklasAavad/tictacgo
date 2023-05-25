90160dd23a8efedd5e2b3abf856b0ea7fd61098f
Niklas <niklasaavad@gmail.com>
2023-05-25 20:08:34 +0200

    refactor: GameInstruction changed from enum to as const

--- frontend/src/api/FrontendApi.ts
+++ frontend/src/api/FrontendApi.ts
@@ -1,13 +1,15 @@
 import { Position } from "../utility/Position"
 
-export enum GameInstruction {
-    START_GAME = "start game",
-    CHOOSE_SQUARE = "choose square",
-    SELECT_CHARACTER = "select character",
-}
+export const GameInstruction = {
+    START_GAME: "start game",
+    CHOOSE_SQUARE: "choose square",
+    SELECT_CHARACTER: "select character",
+} as const;
+
+export type GameInstructionType = typeof GameInstruction[keyof typeof GameInstruction];
 
 // Not used yet, but should (maybe) be used to validate message before sending it
 export type GameMessage = {
-    instruction: GameInstruction,
+    instruction: GameInstructionType,
     content?: Position
 }
